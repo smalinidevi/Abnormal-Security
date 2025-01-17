10fb39c29fd037e054183b4590ce1bda74428367
Malinidevi S <malinidevi.selvaraj@gmail.com>
2025-01-18 02:43:17 +0530

    Delete backend directory

--- backend/backend_app/util.py
+++
@@ -1,9 +0,0 @@
-from rest_framework_simplejwt.tokens import RefreshToken
- 
-def generate_jwt(user):
-    # Generate refresh and access tokens for the user
-    refresh = RefreshToken.for_user(user)
-    return {
-        'access': str(refresh.access_token),
-        'refresh': str(refresh),
-    }
